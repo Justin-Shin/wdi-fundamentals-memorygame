8cf6773d395e874bf8f92c67594bd11219bb69d3
Justin Shin <jjs3b@virginia.edu>
2017-01-13 20:27:37 -0500

    added message box

--- starter-code/main.js
+++ starter-code/main.js
@@ -4,3 +4,9 @@ var cardOne = "King";
 var cardTwo = "King";
 var cardThree = "Queen";
 var cardFour = "Queen";
+
+if (cardOne == cardFour) {
+	alert('You found a match!');
+} else if (cardOne !== cardFour) {
+	alert('Sorry, try again');
+}
